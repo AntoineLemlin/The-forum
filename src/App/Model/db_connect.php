4adf97f39218b8f33d90fdc7cd6eb50cf13b48a8
Yassine Layachi <yassinelayachi@outlook.com>
2021-09-07 16:29:00 +0200

    MVC folder

---
+++ src/App/Model/db_connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$servername="mysql";
+$username="root";
+$password="root";
+$port="3306";
+try{
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=TheForum",$username,$password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected succesfully";
+} catch(PDOException $e){
+    print "Erreur !: " . $e->getMessage() . "<br/>"; 
+}
+
+
+
+?>
