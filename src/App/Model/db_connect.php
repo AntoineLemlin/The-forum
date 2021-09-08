e8ee362d5a304c195ee7a74c2ee3d4a343f4f17b
AntoineLemlin <ant.lemlin@gmail.com>
2021-09-08 14:53:43 +0200

    form register

--- src/App/Model/db_connect.php
+++ src/App/Model/db_connect.php
@@ -6,11 +6,7 @@ $usern="root";
 $passw="root";
 $port="3306";
 try{
-<<<<<<< HEAD
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=TheForum",$usern,$passw);
-=======
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=The-forum",$username,$password);
->>>>>>> dd8c2fa743a0078081174df147a81a92ea6131e1
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=The-forum",$usern,$passw);
     $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected succesfully";
 } catch(PDOException $e){
