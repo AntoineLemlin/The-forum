bfd84d8e7d97cb4a33bced2579a283123fc21ed0
AntoineLemlin <ant.lemlin@gmail.com>
2021-09-08 12:17:04 +0200

    topic

--- src/App/Model/db_connect.php
+++ src/App/Model/db_connect.php
@@ -6,7 +6,7 @@ $username="root";
 $password="root";
 $port="3306";
 try{
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=TheForum",$username,$password);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=The-forum",$username,$password);
     $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected succesfully";
 } catch(PDOException $e){
