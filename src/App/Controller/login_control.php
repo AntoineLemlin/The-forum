0e568891a4a6544878dd332f1aa4feed915f4ca6
jeremilopo <jer.lopopola@gmail.com>
2021-09-08 12:15:11 +0200

    ll

--- src/App/Controller/login_control.php
+++ src/App/Controller/login_control.php
@@ -3,10 +3,26 @@
 if(isset($_POST["login"])){
 
     if(isset($_POST["username"]) && isset($_POST["password"])){
-        echo "Bravo";
+       include_once ("/var/www/html/App/Model/check_login.php");
         $username=$_POST["username"];
         $password=$_POST["password"];
-    }else{
+
+        $user_checkin = new Check_Login();
+        
+        if ($user_checkin -> match_username($username) != false ) {
+              echo "check";
+        } 
+        else {
+            echo "recommence";
+        }
+        
+
+
+    }
+
+    
+    
+    else{
         echo "ERRROR";
     }
 }
--- src/App/Model/check_login.php
+++ src/App/Model/check_login.php
@@ -2,16 +2,46 @@
 
 class Check_Login{
 
-    public function match_username($username){
-        require_once "../Controller/login_check.php";
 
-        $statement = $conn->prepare("SELECT * from TheForum.Users where nickname=:nickname");
-        $statement->execute(['nickname' => $username]); 
-        $id_array = $statement->fetch();
-        $id=$id_array['id'];
+
+    public function match_username($username){
+        require_once ("db_connect.php");
+
+        $statement = $conn->prepare("SELECT * from Users where Nickname=" . $username);
+
+        $statement->execute(); 
+        $username_result = $statement->fetch();
+       
+        
+        if ($username_result != NULL ) {
+            echo $username_result;
+            
+            
+
+        }
+        else {
+            return false  ;
+        }
+
+    
 
 
     }
+/*
+    public function match_password($password){
+        require_once ("db_connect.php");
+       
+       
+       $hash = $this -> match_username($username);
+       
+        if (password_verify($password, $hash)) {
+            echo 'Le mot de passe est valide !';
+        } else {
+            echo 'Le mot de passe est invalide.';
+        }
+
+
+    }*/
 
 
 
