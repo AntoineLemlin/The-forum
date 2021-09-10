42cb306a02c29dadf2bcda9a791b48ef7ba84a73
jeremilopo <jer.lopopola@gmail.com>
2021-09-10 13:52:39 +0200

    topics

--- src/App/Model/db_connect.php
+++ src/App/Model/db_connect.php
@@ -6,7 +6,7 @@ $usern="root";
 $passw="root";
 $port="3306";
 try{
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=TheForum",$usern,$passw);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=Theforum",$usern,$passw);
     $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     print "Erreur !: " . $e->getMessage() . "<br/>"; 
