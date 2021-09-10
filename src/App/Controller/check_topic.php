f9ca9edcf1d5d4612bb0fcd58bcf48bd5b161d23
Yassine Layachi <yassinelayachi@outlook.com>
2021-09-10 12:16:36 +0200

    insert topics

--- src/App/Controller/check_topic.php
+++ src/App/Controller/check_topic.php
@@ -7,12 +7,23 @@
 
 if(isset($_POST["add-topic"])){
 
-
-    $email = $_POST['email']; 
-    echo $email;
     $title= $_POST['title']; 
     $content = $_POST['content']; 
     $board = $_POST['board-name'];
+    date_default_timezone_set("Europe/Paris");
+    $date=date("Y-m-d h:i:s");
+    
+
+    if(!empty($title) || !empty($content) || !empty($board)){
+
+        require "../App/Model/insert_topic.php";
+        $id=find_authorid($_SESSION["username"]);
+        insert_topic($title,$content,$board,$id,$date);
+
+
+    }else{
+        echo "Tous les champs sont obligatoires";
+    }
   
 
      
