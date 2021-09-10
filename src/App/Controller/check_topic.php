97a28f56244751a69a18c675a41609540e314e56
jeremilopo <jer.lopopola@gmail.com>
2021-09-10 11:26:47 +0200

    ll

---
+++ src/App/Controller/check_topic.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+
+
+
+
+if(isset($_POST["add-topic"])){
+
+
+    $email = $_POST['email']; 
+    echo $email;
+    $title= $_POST['title']; 
+    $content = $_POST['content']; 
+    $board = $_POST['board-name'];
+  
+
+     
+}
