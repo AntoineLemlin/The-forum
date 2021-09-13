ea1d996c9cfd9384fce978a5ad3899809a4238d6
jeremilopo <jer.lopopola@gmail.com>
2021-09-13 13:53:21 +0200

    scss

--- src/public/messages.php
+++ src/public/messages.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +9,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
   <link rel="stylesheet" href="styles/style.css">
   <title>Tracker</title>
-</head>
-<?php
+</head><?php
 
   require_once("../App/Controller/check_session.php");
   require_once("../App/Controller/login_control.php");
@@ -58,11 +58,207 @@
       </div>
     </div>
     </header>
-    <main class="mx-5">
+
+<main class="mx-5">
 
 
-</main>
-<footer>
+  <div class="container-fluid" id="main-container">
+  <div class="container-fluid" id="breadcrumb">
+  <nav class="" aria-label="breadcrumb">
+    <ol class="breadcrumb bread-style align-text-bottom">
+      <li class="breadcrumb-item bread-item">
+        <a href="#" class="text-decoration-none text-dark"
+          ><i class="fas fa-home"></i>Home</a
+        >
+      </li>
+      <li class="breadcrumb-item bread-item active" aria-current="page">
+        Board index
+      </li>
+    </ol>
+  </nav>
+      </div>
+      <div class="row">
+        <div class="col-md-9 col-sm col-lg-9">
+          <h1 class="mx-5">Topic Icon Demos</h1>
+         
+        
+          <div class="container-fluid p-5">
+         <div class="row">
+          <div id="forum-rules" class="col-12 border-left border-danger p-3">
+                <h4 class="text-danger">Forum rules</h4>
+              </div>
+          </div>
+         
+          <div class="row">
+              <div class="col-12 d-flex align-items-center justify-content-start m-4">
+
+                <a id="new-message" class="h5 mr-5 text-white bg-primary p-3 rounded-pill" href="new_topics.php">Post-Reply <i class="fas fa-reply"></i></a>
+                
+                <div class="dropdown mr-5 pr-3">
+                  <button class="btn btn-secondary dropdown-toggle rounded-pill  " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-wrench p-0"></i>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </div>
+
+
+
+
+                <form id="search-bar" class="form-inline flex-nowrap" name="search-bar">
+                  <input class="form-control w-100" type="search" placeholder="Search" aria-label="Search">
+                </form>
+              </div>
+            </div>
+            <div class="row">
+              <div class="container">
+                <div class="col-3">
+                  <div class ="img-modifier">
+                    <img src="" alt="">
+                  </div>
+                  <h6>Nom de l'utilisateur</h6>
+                  <p class="user-id"><strong>Posts:</strong>45</p>
+                </div>
+                <div class="col-9">
+
+                </div>
+              </div>
+            </div>
+  <div id="topics" class="row table-responsive mt-5">
+    <table class="table">
+     
+
+        
+
+<main class="mx-5">
+
+    </table>
+</div>
+            <div class="row">
+              
+            </div>
+            <div class="row">
+              <div class="col-12 mt-5 d-flex">
+                <a id="return-board" class="h4 mr-5 text-dark p-3" href="#">&lt; Return to Board Index</a> <a id="jump-to" class="h4 mr-5 text-dark bg-light p-3 ml-auto" href="#">Jump to ^</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-lg-3 px-5">
+          <?php include("../App/View/reduced_profile.php"); ?>
+          <!-- Search Bar -->
+          <form id="search-bar" class="form-inline flex-nowrap">
+          <input class="form-control w-100" type="search"
+          placeholder="Search" aria-label="Search">
+          <button class="btn btn-
+          info" type="submit"><i class="fas fa-search"></i>
+        </button>
+        <button class="btn btn-
+          info" type="submit"><i class="fas fa-cog"></i>
+        </button>
+          </form> 
+          <hr>
+          <?php include("../App/View/form_display.php") ?>
+          <div class="table-responsive mt-5">
+            <table id="table-side" class="table">
+              <thead class="thead">
+                <tr>
+                  <th scope="col" class="h3">Last posts</th>
+                </tr>
+              </thead>
+              <tbody class="bg-light d-flex justify-content-center">
+                <tr>
+                  <td class="card m-4" style="width: 100%;">
+                    <div>
+                      <div class="card-body">
+                        <div class="title d-flex align-items-center">
+                          <h5 class="card-title">Post - Category</h5>
+                          <h6 class="card-subtitle ml-auto text-muted">2 hours ago</h6>
+                        </div>
+                        <p class="card-text">Writing your own post here. It gets displayed here.</p>
+                        <p class="card-text">Tags: test, work, eat, repeat</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="card m-4" style="width: 100%;">
+                    <div>
+                      <div class="card-body">
+                        <div class="title d-flex align-items-center">
+                          <h5 class="card-title">Post - Category</h5>
+                          <h6 class="card-subtitle ml-auto text-muted">2 hours ago</h6>
+                        </div>
+                        <p class="card-text">Writing your own post here. It gets displayed here.</p>
+                        <p class="card-text">Tags: test, work, eat, repeat</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="card m-4" style="width: 100%;">
+                    <div>
+                      <div class="card-body">
+                        <div class="title d-flex align-items-center">
+                          <h5 class="card-title">Post - Category</h5>
+                          <h6 class="card-subtitle ml-auto text-muted">2 hours ago</h6>
+                        </div>
+                        <p class="card-text">Writing your own post here. It gets displayed here.</p>
+                        <p class="card-text">Tags: test, work, eat, repeat</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="card m-4" style="width: 100%;">
+                    <div>
+                      <div class="card-body">
+                        <div class="title d-flex align-items-center">
+                          <h5 class="card-title">Post - Category</h5>
+                          <h6 class="card-subtitle ml-auto text-muted">2 hours ago</h6>
+                        </div>
+                        <p class="card-text">Writing your own post here. It gets displayed here.</p>
+                        <p class="card-text">Tags: test, work, eat, repeat</p>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="table-responsive mt-5">
+            <table id="table-side" class="table">
+              <thead class="thead">
+                <tr>
+                  <th scope="col" class="h3">Last active user</th>
+                </tr>
+              </thead>
+              <tbody class="bg-light">
+                <tr class="d-flex justify-content-center flex-wrap">
+                <?php require("../App/View/last_active_display.php"); ?>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="table-responsive mt-5">
+            <table id="widget-side" class="table">
+              <thead class="thead bg-dark text-white">
+                <tr>
+                  <th scope="col" class="h3">Blank Widget (ALT block)</th>
+                </tr>
+              </thead>
+              <tbody class="bg-light">
+                <tr class="d-flex justify-content-center flex-wrap">
+                  <td id="last-active-card" class="bg-white w-75 my-3">
+                    <h2>Some information goes here :)</h2>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </main>
+  <footer>
   <div class="container-fluid" id="social-media">
         <ul class="d-flex justify-content-center py-5 flex-wrap">
             <li class="p-3 mr-4"><a href="#"><i class="fab fa-instagram text-white"></i></a></li>
