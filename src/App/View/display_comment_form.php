0bc94cee70db6e309435543cbd98ee88addfb7f8
jeremilopo <jer.lopopola@gmail.com>
2021-09-14 10:55:53 +0200

    post

--- src/App/View/display_comment_form.php
+++ src/App/View/display_comment_form.php
@@ -2,12 +2,12 @@
     
     if (isset ($_SESSION['username'])) {
     if(isset($_POST['post-reply'])){
-
+      $topic = $_GET['topic_number'];
 ?>
 
 <div class="col-12">
         <h2 class="text-center mt-5 bg-primary rounded border border-dark p-3 text-white">New comment</h2>
-      <form method="POST" id="add-comment">
+      <form method="POST"    id="add-comment">
           <a class="d-flex justify-content-end text-decoration-none" href="<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="fas fa-times-circle h2"></i></a>
         <div class="form-group">
           <textarea type="text" name="comment" class="form-control" rows="5" id="content" placeholder="Content" required></textarea>
@@ -21,4 +21,21 @@
 
 <?php
     }}
+
+    require "../App/Model/add_comment.php";
+
+    if(isset($_POST['add-comment'])) {
+
+      date_default_timezone_set("Europe/Paris");
+      $topic = $_GET['topic_number'];
+      $author = $_SESSION['id'];
+      $content = $_POST['comment'];
+      $date = date("Y-m-d h:i:s"); 
+
+     add_comment($topic,$date,$author,$content) ;  
+     
+
+    }
+
+
 ?>
